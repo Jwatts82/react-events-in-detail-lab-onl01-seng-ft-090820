8b9f7a67f92867b923d0c84a72e67d945dbb195b
jwatts82 <jessperr182@hotmail.com>
2021-01-12 12:53:49 -0800

    commit tree 131a6adebbaa11968ebdea3753637c0f6fab9d49

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,20 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+class CoordinatesButton extends React.Component {
+    
+    handleClick = (event) => {
+        event.persist()
+        setTimeout( () => {this.props.onDelayedClick(event)}, this.props.timeout)
+    }
+
+    render() {
+        return (
+            <div>
+                <button type="button" onClick={this.handleClick} />
+            </div>
+        )
+    }
+}
+
+export default CoordinatesButton;
